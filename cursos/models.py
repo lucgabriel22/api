e7906df2a04634fb34a26dd7e349a9b3909bac9b
luc <lucasgabriel20182005@gmail.com>
2024-04-15 16:47:07 -0300

    :bricks: criado o models do aplicativo cursos

--- cursos/models.py
+++ cursos/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Base(models.Model):
+    criacao = models.DateField(
+        auto_now=True,
+    )
+    atualizacao = models.DateField(
+        auto_now=True
+    )
+    ativo = models.BooleanField(
+        default=True
+    )
+
+    class Meta:
+        abstract = True
+    
+
+class Curso(Base):
+    titulo = models.CharField(
+        max_length=255
+    )
+    url = models.URLField(
+        unique=True
+    )
+
+    class Meta:
+        verbose_name= 'Curso'
+        verbose_name_plural = 'Cursos'
+
+        def __str__(self):
+            return self.titulo
--- escola/settings.py
+++ escola/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+INSTALLED_APPS += [
+    'cursos'
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -118,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
