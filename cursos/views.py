b01dc0f7428e8be3bb472762bf18d8009e846324
Lucas Gabriel <lucasgabriel20182005@gmail.com>
2024-04-16 19:56:25 -0300

    :construction: adicionado a views e urls

--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
+
+class CursoAPIView(APIView):
+    
+    "API de Cursos"
+    
+    def get(self, request):
+        print(dir(request))
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+    
+
+class AvaliacaoAPIView(APIView):
+
+    "API de Avalições"
+
+    def get(self, request):
+        avalicoes = Avaliacao.objects.all()
+        serializer = AvaliacaoSerializer(avalicoes, many=True)
+        return Response(serializer.data)
