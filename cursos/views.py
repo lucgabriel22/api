7f9a47816f03a09bf6dd3532afd666aa912c9b67
Lucas Gabriel <lucasgabriel20182005@gmail.com>
2024-04-17 19:57:06 -0300

    :zap: código mais clean nas views

--- cursos/views.py
+++ cursos/views.py
@@ -1,40 +1,28 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-
+from rest_framework import generics
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
-class CursoAPIView(APIView):
-    
-    "API de Cursos"
-    
-    def get(self, request):
-        print(dir(request))
-        cursos = Curso.objects.all()
-        serializer = CursoSerializer(cursos, many=True)
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = CursoSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({"status": "adicionado com sucesso!"}, status=status.HTTP_201_CREATED)
-    
 
-class AvaliacaoAPIView(APIView):
+# LISTAR E CRIAR
+class CursosAPIView(generics.ListCreateAPIView):
 
-    "API de Avalições"
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
 
-    def get(self, request):
-        avalicoes = Avaliacao.objects.all()
-        serializer = AvaliacaoSerializer(avalicoes, many=True)
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = AvaliacaoSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({"status": "adicionado com sucesso!"}, status=status.HTTP_201_CREATED)
-    
-    
+#ATUALIZA E DESTROI
+class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+# LISTAR E CRIAR
+class AvaliacaoAPIView(generics.ListCreateAPIView):
+
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+
+#ATUALIZA E DESTROI
+class AvaliacoesAPIView(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
